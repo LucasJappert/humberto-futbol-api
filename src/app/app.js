ca4782e7f347ef37bd740c92106af0ec923da9c3
LucasJappert <ljappert@agroideassa.com>
2023-09-09 10:12:08 -0300

    arreglar cors

--- src/app/app.js
+++ src/app/app.js
@@ -20,13 +20,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(logger);
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');//TODO: Setear ciertos dominios para que no quede expuesta a cualquiera
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE, PATCH');
-    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');//TODO: Setear ciertos dominios para que no quede expuesta a cualquiera
+//     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE, PATCH');
+//     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+//     next();
+// });
 app.get("/", (req, res) => {
     res.status(200).json(`Hello ${process.env.NODE_ENV}! App versión: ${pkg.version}`);
 });
